aa063e612f012d5461ecba79ab96d04cddc78c88
Elissa <elissadesigner@gmail.com>
2023-06-03 20:37:49 +0200

    fix: routes fixing

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
-// vite.config.js
-import reactRefresh from "@vitejs/plugin-react-refresh";
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()],
-  build: {
-    outDir: "dist", // Specify the output directory for the build
-    assetsInlineLimit: 0, // Remove the file size limit for inlining assets
-    rollupOptions: {
-      input: {
-        main: "./index.html", // Entry point of your application
-      },
-    },
-  },
+  plugins: [react()],
 });
