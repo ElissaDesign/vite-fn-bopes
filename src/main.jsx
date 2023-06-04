0c4d1e3df194ec976b9c9f5fdefb68159d825ca1
Elissa <elissadesigner@gmail.com>
2023-06-04 14:07:51 +0200

    rempved

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { ChakraProvider } from "@chakra-ui/react";
-import "react-app-polyfill/ie11";
 import App from "./App.jsx";
 import "./index.css";
 
